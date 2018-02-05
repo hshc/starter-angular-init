969a16d3c185a94cc58d34e118d7cde31dbf20b3
hshc <nico.taroy@gmail.com>
2018-02-05 16:50:20 +0100

    nav bar

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
 
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from './shared/shared.module';
 
+import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 // import { environment } from '../environments/environment.prod';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
     RouterModule,
+    SharedModule,
+    HomeModule
   ],
   declarations: [
     AppComponent
